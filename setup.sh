f9fcb3d8b3bc2fc59bb95923b257940db003d3ff
Giuseppe Ferrari <ibeppo993@gmail.com>
2021-10-03 22:10:38 +0200

    test

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "
+[general]n
+email = "ibeppo993@gmail.com"n
+" > ~/.streamlit/credentials.toml
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
+" > ~/.streamlit/config.toml
